329b5fa3ba3e8e2e796af799bd9a794d69905340
dvndrsingh786 <dvndrsingh786@gmail.com>
2023-04-28 23:58:00 +0100

    Race Track Added! A lot happened

--- Assets/_Dav/Scripts/MenuScript.cs
+++ Assets/_Dav/Scripts/MenuScript.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 namespace Dav
 {
     public class MenuScript : MonoBehaviour
     {
-        public GameObject[] carsObjects;
-        public Transform[] carPositions;
+        public Transform carsParent;
+        public Transform carPositionsParent;
         public int currentSelectedCarIndex;
 
         private void Start()
@@ -15,10 +16,58 @@ namespace Dav
             currentSelectedCarIndex = PlayerData.instance.selectedCarIndex;
         }
 
-        void SetCarWithIndex()
-        {
+        #region Cars Setup
 
+        void InitialCarSetup()
+        {
+            int centerPoint = (carPositionsParent.childCount - ((carPositionsParent.childCount - 1) / 2)) - 1;
+            for (int i = 0; i < currentSelectedCarIndex; i++)
+            {
+                carsParent.GetChild(i).localPosition = carPositionsParent.GetChild(centerPoint - currentSelectedCarIndex + i).localPosition;
+            }
+            int temp = 1;
+            for (int i = currentSelectedCarIndex+1; i < carsParent.childCount; i++)
+            {
+                carsParent.GetChild(i).localPosition = carPositionsParent.GetChild(centerPoint + temp).localPosition;
+                temp++;
+            }
+            carsParent.GetChild(currentSelectedCarIndex).localPosition = carPositionsParent.GetChild(centerPoint).localPosition;
         }
+
+        public void SelectRightCarBtnClicked()
+        {
+            currentSelectedCarIndex++;
+            if (currentSelectedCarIndex >= carsParent.childCount) currentSelectedCarIndex--;
+            SetUpCarWithIndex();
+        }
+
+        public void SelectLeftCarBtnClicked()
+        {
+            currentSelectedCarIndex--;
+            if (currentSelectedCarIndex < 0) currentSelectedCarIndex = 0;
+            SetUpCarWithIndex();
+        }
+
+        void SetUpCarWithIndex()
+        {
+            int centerPoint = (carPositionsParent.childCount - ((carPositionsParent.childCount - 1) / 2)) - 1;
+            for (int i = 0; i < currentSelectedCarIndex; i++)
+            {
+                carsParent.GetChild(i).DOLocalMove(carPositionsParent.GetChild(centerPoint - currentSelectedCarIndex + i).localPosition, 0.3f);
+            }
+            int temp = 1;
+            for (int i = currentSelectedCarIndex + 1; i < carsParent.childCount; i++)
+            {
+                carsParent.GetChild(i).DOLocalMove(carPositionsParent.GetChild(centerPoint + temp).localPosition, 0.3f);
+                temp++;
+            }
+            carsParent.GetChild(currentSelectedCarIndex).DOLocalMove(carPositionsParent.GetChild(centerPoint).localPosition, 0.3f);
+        }
+
+        #endregion
+
+
+
 
     }
 }
